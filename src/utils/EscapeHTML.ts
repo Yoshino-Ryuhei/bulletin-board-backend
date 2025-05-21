a0e1af1d47b9b550aed6a47a7e648d1d9ccb6d55
Yoshino-ryuhei <fightworking0801@gmail.com>
2025-05-21 16:56:37 +0900

    post修正&セキュリティ対策

---
+++ src/utils/EscapeHTML.ts
@@ -0,0 +1,10 @@
+function escapeHTML(str: string): string {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+export { escapeHTML };
